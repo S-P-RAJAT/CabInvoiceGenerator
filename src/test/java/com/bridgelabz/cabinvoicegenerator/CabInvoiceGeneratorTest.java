7b8aeb3b257ac0998885add029761e764f90490b
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-23 00:26:37 +0530

    ADDED: Test case to validate invoice generated with premium rides

--- src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -18,24 +18,24 @@ public class CabInvoiceGeneratorTest {
 
     @Test
     public void givenDistanceAndTime_ReturnTotalFare() {
-        
+
         double distance = 5.0;
         int time = 9;
-        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        double fare = cabInvoiceGenerator.calculateFare(distance, time,RideType.NORMAL);
         Assert.assertEquals(59, fare, 0.0);
     }
 
     @Test
     public void givenLessDistanceAndTime_ReturnMinimumFare() {
-        
+
         double distance = 0.1;
         int time = 1;
-        double fare = cabInvoiceGenerator.calculateFare(distance,time);
+        double fare = cabInvoiceGenerator.calculateFare(distance,time,RideType.NORMAL);
         Assert.assertEquals(5, fare, 0.0);
     }
     @Test
     public void givenMultipleRides_ReturnInvoiceSummary(){
-        
+
         ArrayList<Ride> rides = new ArrayList<>();
         rides.add(new Ride(2.0, 5));
         rides.add(new Ride(0.01, 1));
@@ -48,9 +48,22 @@ public class CabInvoiceGeneratorTest {
         String[] userIdList = { "Ram", "Rahul", "Raghav" };
         Ride[][] rides = { { new Ride(5.0, 12), new Ride(2.5, 6) }, { new Ride(3.0, 5), new Ride(0.01, 1) },
                 { new Ride(10.0, 15), new Ride(2, 30) } };
+
         cabInvoiceGenerator.addRideToRepository(userIdList, rides);
         InvoiceSummary summary = cabInvoiceGenerator.invoiceForUser(userIdList[2]);
         InvoiceSummary expectedSummary = new InvoiceSummary(rides[2].length, 165.0);
         Assert.assertEquals(expectedSummary, summary);
     }
+    @Test
+    public void givenPremiumTypeAndUserId_ReturnInvoiceSummary(){
+        String[] userIdList = { "Ram", "Rahul", "Raghav" };
+        Ride[][] rides = { { new Ride(5.0, 12,RideType.PREMIUM), new Ride(2.5, 6) }, { new Ride(3.0, 5), new Ride(0.01, 1) },
+                { new Ride(10.0, 15,RideType.PREMIUM), new Ride(2, 30,RideType.PREMIUM) } };
+
+        cabInvoiceGenerator.addRideToRepository(userIdList, rides);
+        InvoiceSummary summary = cabInvoiceGenerator.invoiceForUser(userIdList[2]);
+        InvoiceSummary expectedSummary = new InvoiceSummary(rides[2].length, 270.0);
+        Assert.assertEquals(expectedSummary, summary);
+    }
+
 }
