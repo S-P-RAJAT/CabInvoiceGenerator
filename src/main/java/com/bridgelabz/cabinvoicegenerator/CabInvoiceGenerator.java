e88c470fbb3238f21ddfcbf8eeb81dbf27848552
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-22 17:45:32 +0530

    MODIFIED: Logic to calculate fare is written to pass the test case

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -2,8 +2,12 @@ package com.bridgelabz.cabinvoicegenerator;
 
 public class CabInvoiceGenerator {
 
+    private static final double COST_PER_KM = 10.0;
+    private static final int COST_PER_MINUTE = 1;
+    private static final double MIN_FARE = 5.0;
 
     public double calculateFare(double distance, int time) {
-        return 0;
+        double totalFare = distance * COST_PER_KM + time * COST_PER_MINUTE;
+        return Math.max(totalFare, MIN_FARE);
     }
 }
