fcb383d8f5aa783cfc00156c5a42c10ed2b4dc91
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-23 00:29:25 +0530

    ADDED: Logic to generate invoice for premium rides

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -4,36 +4,38 @@ import java.util.List;
 
 public class CabInvoiceGenerator {
 
-    private static final double COST_PER_KM = 10.0;
-    private static final int COST_PER_MINUTE = 1;
-    private static final double MIN_FARE = 5.0;
     RideRepository rideRepository;
 
     public CabInvoiceGenerator() {
         rideRepository = new RideRepository();
     }
-    public double calculateFare(double distance, int time) {
-        double totalFare = distance * COST_PER_KM + time * COST_PER_MINUTE;
-        return Math.max(totalFare, MIN_FARE);
+
+    public double calculateFare(double distance, int time, RideType rideType) {
+        double totalFare = distance * rideType.getCostPerKm() + time * rideType.getCostPerMinute();
+        return Math.max(totalFare, rideType.getMinimumFare());
     }
+
     public double calculateFare(List<Ride> rides) {
         double totalFare = 0;
         for (Ride ride : rides) {
-            totalFare += calculateFare(ride.distance,ride.time);
+            totalFare += calculateFare(ride.distance, ride.time, ride.rideType);
         }
+        System.out.println(totalFare);
         return totalFare;
     }
+
     public InvoiceSummary returnRideSummary(List<Ride> rides) {
         double totalFare = calculateFare(rides);
         int numberOfRides = rides.size();
         return new InvoiceSummary(numberOfRides, totalFare);
     }
+
     public InvoiceSummary invoiceForUser(String userId) {
         return returnRideSummary(rideRepository.getRidesForUser(userId));
     }
 
-    public void addRideToRepository(String[] userIdList, Ride[][] rides){
-        for(int i = 0; i < userIdList.length; i++) {
+    public void addRideToRepository(String[] userIdList, Ride[][] rides) {
+        for (int i = 0; i < userIdList.length; i++) {
             for (int j = 0; j < rides[i].length; j++) {
                 rideRepository.addRideForUser(userIdList[i], rides[i][j]);
             }
