82fad708cdaba91c79235743529f30ada8884d66
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-22 17:44:15 +0530

    ADDED: Test case before writing logic to follow test driven development

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class CabInvoiceGenerator {
+
+
+    public double calculateFare(double distance, int time) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CabInvoiceGeneratorTest {
+
+    @Test
+    public void givenDistanceAndTime_ReturnTotalFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 5.0;
+        int time = 9;
+        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(59, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ReturnMinimumFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 0.01;
+        int time = 1;
+        double fare = cabInvoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
+}
